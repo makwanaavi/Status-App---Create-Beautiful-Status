819b46f8cba3e4a1bff3ef99834a16e26474301d
prabhu-woyce <pparmar@woyce.io>
2025-08-25 17:03:32 +0530

    #update

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,6 +2,40 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import { thunk } from "redux-thunk";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// Load persisted state from localStorage
+function loadState() {
+  try {
+    const serializedState = localStorage.getItem("reduxState");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    return undefined;
+  }
+}
+
+// Save state to localStorage
+function saveState(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("reduxState", serializedState);
+  } catch (e) {
+    // Ignore write errors
+  }
+}
+
+const persistedState = loadState();
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  applyMiddleware(thunk)
+);
+
+// Subscribe to store changes and persist
+store.subscribe(() => {
+  // Only persist relevant slices
+  const { status, user, editor } = store.getState();
+  saveState({ status, user, editor });
+});
 
 export default store;
